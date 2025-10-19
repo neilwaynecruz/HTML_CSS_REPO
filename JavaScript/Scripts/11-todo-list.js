ab6e308215e0e14e6079642eea2a71adea4778a4
Neil Wayne <neilwayne0514@gmail.com>
2025-10-19 12:49:50 +0800

    Javascript 31th commit

--- JavaScript/Scripts/11-todo-list.js
+++ JavaScript/Scripts/11-todo-list.js
@@ -1,5 +1,7 @@
 let todoList1 = []; // to store the todo items
 let todoList2 = [];
+let todoList3 = [];
+
 renderTodoSimple2(); // to render the initial empty list
 
 function addTodoSimple() {
@@ -13,7 +15,6 @@ function addTodoSimple() {
 
   todoList1.push(toDoValue);
   document.querySelector(".js-todo-name-simple").value = "";
-  console.log(todoList1);
   return;
 }
 function addTodoSimple2() {
@@ -27,7 +28,6 @@ function addTodoSimple2() {
 
   todoList2.push(toDoValue);
   document.querySelector(".js-todo-name-simple-2").value = "";
-  console.log(todoList2);
 
   // call the render function to update the UI
   renderTodoSimple2();
@@ -39,9 +39,51 @@ function renderTodoSimple2() {
   // loop through the array and create a <p> element for each todo
   for (let i = 0; i < todoList2.length; i++) {
     toDoListHTML += `
-          <p>${todoList2[i]}</p>
+          <p>
+            ${todoList2[i]}
+          </p>
+          `;
+  }
+  document.querySelector(".js-todo-list-simple-2").innerHTML = toDoListHTML; // to update the UI
+}
+
+function addTodoSimple3() {
+  const toDoValue = document.querySelector(".js-todo-name-simple-3").value;
+
+  // to check if the input is empty
+  if (toDoValue === "") {
+    alert("Enter some valid values");
+    return;
+  }
+  const date = document.querySelector(".js-due-date-input").value;
+
+  todoList3.push({
+    name: toDoValue,
+    dueDate: date,
+  });
+  document.querySelector(".js-todo-name-simple-3").value = "";
+  document.querySelector(".js-due-date-input").value = "";
+
+  // call the render function to update the UI
+  renderTodoSimple3();
+}
+
+function renderTodoSimple3() {
+  let toDoListHTML = "";
+
+  // loop through the array and create a <p> element for each todo
+  for (let i = 0; i < todoList3.length; i++) {
+    const { name, dueDate } = todoList3[i];
+    toDoListHTML += `
+          <div>${name}</div>
+          <div>${dueDate}</div>
+          <button onclick="
+            todoList3.splice(${i}, 1);
+            renderTodoSimple3();
+          " class="cs-delete-btn">Delete</button>
+          
           `;
   }
 
-  document.querySelector(".js-todo-list-simple-2").innerHTML = toDoListHTML; // to update the UI
+  document.querySelector(".js-todo-list-simple-3").innerHTML = toDoListHTML; // to update the UI
 }
